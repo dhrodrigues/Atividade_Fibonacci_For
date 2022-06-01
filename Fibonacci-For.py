3a49fa73fbb01f201912c2b6d6c6796337e78345
Diego Rodrigues <diego17ar@hotmail.com>
2022-06-01 19:16:12 -0300

    Atualização correção para que o codigo possa rodar em loop

--- Fibonacci-For.py
+++ Fibonacci-For.py
@@ -1,18 +1,34 @@
-termos = int(input("Qual numero termo?\n"))
+termos = int(input("Informe quantos termos da sequencia Fibonacci deseja\n"))
 
-x = 0
-y = 1
+#Estrutura de repetição
+while(termos !=0):
 
-if termos == 0:
-    print("Final")
+    #Declaração dos primeiros numeros
+    x = 0
+    y = 1
+    
+    
+    if termos == 1:
+        print("\n",x)
 
-else:
-    print("Sequencia Fibonacci:")
-    print(x)
-    print(y)
-    for n in range(2, termos):
-        z = x + y
-        x = y
-        y = z
-        print (z)
+    elif termos == 2:
+        print("\n",x,"\n",y)
 
+    elif termos == 3:
+        print("\n",x,"\n",y,"\n",x+y)
+
+    else:
+        print("Sequencia Fibonacci:")
+        print(x)
+        print(y)
+
+        #Loop em for onde é realizada a soma apartir do 
+        for n in range(2, termos):
+            z = x + y
+            x = y
+            y = z
+            print (z)
+    print("-"*20)
+
+    termos = int(input("Digite qual a nova sequencia Fibonacci. Caso digite 0 sera encerrado o codigo\n"))
+print("Tchau obrigado por utilizar esse Script")
